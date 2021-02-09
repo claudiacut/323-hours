b790fa2820f095a2f29345b40ae657da79e91e22
claudiacut <claudiamaria.cutrupi@mail.polimi.it>
2021-02-09 16:27:26 +0100

    NONNEPOSSO

--- js/random.js
+++ js/random.js
@@ -8,6 +8,10 @@ function getItem(){
     var index=Math.random() * (max - min) + min;
     index=index |0;
     console.log(index);
-    document.getElementById("shuffle").innerHTML = '<div class="content-wrap caso" id='+index.toString()+'>  '+ document.getElementById(index.toString()).innerHTML+'	</div>' ;
+    document.getElementById("shuffle").innerHTML = '<div class="content-wrap caso" id='+index.toString()+'>  '+ document.getElementById(index.toString()).innerHTML+'	</div>';
 
 }
+
+// function off(){
+// document.getElementById("shuffle").innerHTML.onclick =  document.getElementById("0").style.display = "none";
+// }
