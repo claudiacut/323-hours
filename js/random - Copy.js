b1214f376f04643e8f15012de0736f54dfdecfe4
claudiacut <claudiamaria.cutrupi@mail.polimi.it>
2021-02-09 22:01:59 +0100

    funonzia

--- js/random - Copy.js
+++ js/random - Copy.js
@@ -3,12 +3,12 @@
 
 
 function getItem(){
-   var max=16;
+    var max=3;
     var min=1;
     var index=Math.random() * (max - min) + min;
     index=index |0;
     console.log(index);
-    document.getElementById("shuffle").innerHTML = '<div class="content-wrap caso" id='+index.toString()+'>  '+ document.getElementById(index.toString()).innerHTML+'	</div>';   
+    document.getElementById("shuffle").innerHTML = '<div class="content-wrap caso" id='+index.toString()+'>  '+ document.getElementById(index.toString()).innerHTML+'	</div>';
 
 }
 
